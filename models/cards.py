88846b88ffd6e1bd33651851b4346f1e3b983dd5
Tommi Rintala <tommi.rintala@delektre.fi>
2018-10-21 13:55:14 +0300

    added descriptions

--- models/cards.py
+++ models/cards.py
@@ -4,6 +4,7 @@ from odoo import api, fields, models, _
 
 class HrTravelSupportLoyaltyCard(models.Model):
     _name = "hr_travelsupport.loyaltycard"
+    _description = 'Loyaltycard'
     employee_id = fields.Many2one('hr.employee', string='Employee',
                                   ondelete='cascade', index=True)
     name = fields.Selection(
--- models/employee.py
+++ models/employee.py
@@ -4,6 +4,7 @@ from odoo import api, fields, models, _
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
+    _description = 'Employee'
     loyaltycard_ids = fields.One2many(
         'hr_travelsupport.loyaltycard',
         'employee_id',
