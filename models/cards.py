e8a6836941b376bd07c9687775c1b255eb90a720
Tommi Rintala <tommi.rintala@delektre.fi>
2018-10-21 13:13:40 +0300

    Typofix

--- models/cards.py
+++ models/cards.py
@@ -3,16 +3,17 @@
 from odoo import api, fields, models, _
 
 class HrTravelSupportLoyaltyCard(models.Model):
-    name = fields.Selection(string='Card',
-                            [
-                                'Finnair',
-                                'Lufthansa/Miles &amp; More',
-                                'Norwegian',
-                                'Scandic',
-                                'Avis',
-                                'Hertz',
-                                'S-Card',
-                                ]
-                            )
+    name = fields.Selection(
+        [
+            'Finnair',
+            'Lufthansa/Miles &amp; More',
+            'Norwegian',
+            'Scandic',
+            'Avis',
+            'Hertz',
+            'S-Card',
+        ],
+        string='Card',
+    )
     number = fields.Char('Number')
 
