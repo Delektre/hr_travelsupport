4bf0526ef6f0899c43510395a4f9f4a18f84aa96
Tommi Rintala <tommi.rintala@delektre.fi>
2018-10-21 13:41:43 +0300

    published view

--- __manifest__.py
+++ __manifest__.py
@@ -5,8 +5,9 @@
 # noinspection PyStatementEffect
 {
     "name": "HR - Travel support (simple)",
-    "category": "Localization",
+    "category": "Human Resources",
     "version": "0.0.1",
+    "summary": "Travelsupport (Simple)",
     "description": """
     Simple travel support extension.
 
@@ -18,6 +19,7 @@
         "hr",
     ],
     "data": [
+        'views/hr_travelsupport_views.xml',
     ],
     "installable": True,
 }
